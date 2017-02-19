ab691cfb508a815ebfbf3586850dc14519eed925
Andrew Kelly <kellyaj@gmail.com>
2017-02-18 19:02:28 -0800

    basic script proof of concept

--- workwidth.sh
+++ workwidth.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-
-timestampDelimiter='----'
-startDate=`date +%s`
 startDateHuman=`date +%Y-%m-%d-%T`
-startingMeasure=`netstat -I en0 -ib | awk '{print $7,$10}' | tail -1`
 sessionFileName="ww-${startDateHuman}.txt"
-printf "creating session file ${sessionFileName}\n"
-touch $sessionFileName
-echo $startDate >> $sessionFileName
-echo "$startingMeasure" >> $sessionFileName
-echo $timestampDelimiter >> $sessionFileName
+
+function initializeSession {
+  printf "creating session file ${sessionFileName}\n"
+  touch $sessionFileName
+  printf "adding initial reading to ${sessionFileName}\n"
+  startingMeasure=$(takeMeasure)
+  echo "$startingMeasure" >> $sessionFileName
+}
+
+function takeMeasure {
+  timestamp=$(date +%s)
+  rawMeasure=`netstat -I en0 -ib | awk '{print $7,$10}' | tail -1`
+  currentIn=$(echo $rawMeasure | cut -d " " -f 1)
+  currentOut=$(echo $rawMeasure | cut -d " " -f 2)
+  echo "{\"timestamp\":\"${timestamp}\", \"in\":\"${currentIn}\", \"out\":\"${currentOut}\"}"
+}
+
+function runMeasureLoop {
+  (while true; do sleep 60 && echo $(takeMeasure) >> $sessionFileName; done)&
+  echo $!
+}
+
+initializeSession
+runMeasureLoop
